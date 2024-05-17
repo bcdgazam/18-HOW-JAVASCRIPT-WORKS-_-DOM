f43aa02741b07be2d60e05a4ef32afce55d21517
Golam Azam <bcdazam@gmail.com>
2024-05-17 22:02:06 +0600

    DOm

---
+++ 18-3.js
@@ -0,0 +1,18 @@
+//18-3 How Javascript Run and why script order is important
+
+/* How Js Works
+
+    > It's a high-level programming language a single threaded and non blocking or Interpreted programming language.
+
+    > Script: Who can run itself;
+
+    > interpreter: Who can understand everything.
+
+    > Compiler: Who process.
+
+    > non - blocking:One by one execute করে। উপর থেকে লোড হয়ে যেখানে ব্যাবহার করা হয়েছে সেখানে থেমে যাবে লোড হওয়ার পর পরের অংশে যাবে। 
+
+*/
+
+debugger; // এটা যেখানে ব্যাবহার করব সেই পর্যন্ত এসে লোড থেমে যাবে এবং ব্রাউজারে একটা নোটস দেখাবে।
+
--- DomEvents/scripts/script.js
+++ DomEvents/scripts/script.js
@@ -1,1 +1,2 @@
 console.log(90);
+debugger;

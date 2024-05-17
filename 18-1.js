f1a7efab6c2cf4d96dd6687c63c122be3f9c06c5
Golam Azam <bcdazam@gmail.com>
2024-05-17 11:51:04 +0600

    practice

---
+++ 18-1.js
@@ -0,0 +1,7 @@
+//18-1 DOM and Events Module Introduction
+
+/*
+    What is js and where/how it uses
+    ==> Browser এ জেস অফ করে দিলে দেখা যাবে অনেক কিছুই মিস করতেছে। যে এস হল ওয়েব সাইটের প্রান।
+*/
+

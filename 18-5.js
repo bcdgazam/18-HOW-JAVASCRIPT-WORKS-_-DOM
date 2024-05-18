e815886fe928a0e71d14e07bc96d325aafbc8ae6
Golam Azam <bcdazam@gmail.com>
2024-05-18 11:51:31 +0600

    getElementById Practice

---
+++ 18-5.js
@@ -0,0 +1,19 @@
+//18-5 Capture Elements from HTML File using getElementBy Id-
+
+
+/*
+    see the dom.html file for practice.
+
+    ====<>====
+    Open The browser > open console tab > document.(there are a lot of tag)
+    
+        <script>
+            // document.getElementsByTagName('h3');
+            let first = document.getElementById('first');
+            first.style.backgroundColor = '#ddd';
+            first.style.color = 'green';
+            first.style.fontSize = '30px';
+        </script>
+
+    document.getElementsByTagName('h3') ব্রাউজারে এভাবে লিখতে হয়।
+*/

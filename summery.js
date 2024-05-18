41e9ce636f49e369e6642acdde2d09a66dc5a8f0
Golam Azam <bcdazam@gmail.com>
2024-05-18 20:33:52 +0600

    DOM Finish

---
+++ summery.js
@@ -0,0 +1,14 @@
+/*
+
+    18-1 DOM and Events Module Introduction
+    18-2 Create Script tag and connect external Script file
+    18-3 How Javascript Run and why script order is important
+    18-4 What is DOM (Document Object Model)
+    18-5 Capture Elements from HTML File using getElementBy Id-
+    18-6 How to use getElementsByClassName and change innerHTML
+    18-7 How to use querySelector and querySelectorAll
+    18-8 Node, node type, nodelist, htmlcollection, setAttribute
+    18-9 How to add elements to HTML using Javascript
+    18-10 Module summary Javascript and DOM-
+
+*/

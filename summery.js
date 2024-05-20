88e7a4f86efc14a06d4298873c007c475a7f1956
Golam Azam <bcdazam@gmail.com>
2024-05-20 11:47:02 +0600

    DOM practice

--- summery.js
+++ summery.js
@@ -4,7 +4,7 @@
     18-2 Create Script tag and connect external Script file
     18-3 How Javascript Run and why script order is important
     18-4 What is DOM (Document Object Model)
-    18-5 Capture Elements from HTML File using getElementBy Id-
+    18-5 Capture Elements from HTML File using getElementBy Id- (Taka an ID, Select it and style it.)
     18-6 How to use getElementsByClassName and change innerHTML
     18-7 How to use querySelector and querySelectorAll
     18-8 Node, node type, nodelist, htmlcollection, setAttribute

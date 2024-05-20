892c6037812b6c0aad9c26dc0e087d5377873031
Golam Azam <bcdazam@gmail.com>
2024-05-20 13:44:18 +0600

    practice

--- summery.js
+++ summery.js
@@ -5,10 +5,9 @@
     18-3 How Javascript Run and why script order is important
     18-4 What is DOM (Document Object Model)
     18-5 Capture Elements from HTML File using getElementBy Id- (Taka an ID, Select it and style it.)
-    18-6 How to use getElementsByClassName and change innerHTML
+    18-6 How to use getElementsByClassName and change innerHTML (change style by using getElementsByClassName with for loop also change heading by js)
     18-7 How to use querySelector and querySelectorAll
     18-8 Node, node type, nodelist, htmlcollection, setAttribute
     18-9 How to add elements to HTML using Javascript
-    18-10 Module summary Javascript and DOM-
 
 */
